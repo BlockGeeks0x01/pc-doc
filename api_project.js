008201407bf35adcc45887cd17a4a5d04840a05e
sunguyu <sunguyu@123feng.com>
2017-03-21 20:01:54 +0800

    update doc

---
+++ api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "联合采购系统API",
+  "version": "1.0.0",
+  "description": "purchasing-consortia api doc",
+  "title": "联合采购 API Doc",
+  "order": [
+    "Product"
+  ],
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-03-21T12:01:53.263Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.1"
+  }
+});
