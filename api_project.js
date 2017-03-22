1b12dafb1117aeb4ef75d85fbb8faad5126f4be2
sunguyu <sunguyu@123feng.com>
2017-03-22 17:40:56 +0800

    update doc

--- api_project.js
+++ api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-03-22T01:27:51.806Z",
+    "time": "2017-03-22T09:40:56.223Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
