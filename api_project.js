1704bf0b07c26a560fdee5b5df27bc779b1ebaf7
EricSun <ericsgy@163.com>
2021-04-06 10:48:38 +0800

    update doc

--- api_project.js
+++ api_project.js
@@ -1,1 +1,34 @@
-define({  "name": "联合采购系统API",  "version": "1.0.0",  "description": "purchasing-consortia api doc",  "title": "联合采购 API Doc",  "order": [    "Constant",    "Structure",    "User",    "Customer",    "Product",    "Order",    "Org",    "Channel",    "Notification",    "CalendarEvent",    "Search",    "Log",    "Config",    "File",    "Feedback",    "Time",    "Pay"  ],  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-10-14T08:59:01.664Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
+define({
+  "name": "联合采购系统API",
+  "version": "1.0.0",
+  "description": "purchasing-consortia api doc",
+  "title": "联合采购 API Doc",
+  "order": [
+    "Constant",
+    "Structure",
+    "User",
+    "Customer",
+    "Product",
+    "Order",
+    "Org",
+    "Channel",
+    "Notification",
+    "CalendarEvent",
+    "Search",
+    "Log",
+    "Config",
+    "File",
+    "Feedback",
+    "Time",
+    "Pay"
+  ],
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-04-06T02:48:37.607Z",
+    "url": "https://apidocjs.com",
+    "version": "0.27.1"
+  }
+});
